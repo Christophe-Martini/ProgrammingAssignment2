114565c04ae78f9e5b36ae5947231e1af8eda499
Christophe <christophe.martini@syscora.fr>
2014-12-18 23:37:53 +0100

    updated

--- cachematrix.R
+++ cachematrix.R
@@ -3,7 +3,15 @@
 ## ********************************
 ##  the makeCacheMatrix function returns a list of four functions 
 ##  to handle an encpsulated matrix. 
+##  One of them allows to get the inverse of the current matrix
+##  However it returns NULL if the makeCacheMatrix function has not been called
+##  before.
 
+##  Indeed,the makeCacheMatrix function is in charge of calculating only once,
+##  the inverse of the given makeCacheMatrix object as its argument.
+##  When this function is called twice and more, it returns a caching result, 
+##  thus to increase the performance. It's particularly fine
+##  when this function handles big square matrix.
         
 ## ********************************
 ## Write a short comment describing this function
@@ -31,6 +39,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 
 ## Write a short comment describing this function
+##  makeCacheMatrix calculates the inverse of the makeCacheMatrix object given
+##  as its argument.
+##  When cacheSolve is called for calculating the inverse of this given matrix,
+##  it first calls the getInv() function upon this matrix object so as to verify
+##  if this instance has its inverse already calculated.
+##  If this is the case, cacheSolve only returns this caching result, 
+##  otherwise it calculates for the first time the inverse
+##  of this makeCacheMatrix object.
+
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
